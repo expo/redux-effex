ab5d77984ab4a9db83b9b1bf1d3a4169b6af8360
Brent Vatne <brentvatne@gmail.com>
2016-09-10 16:38:05 -0700

    Use index.js.flow instead of flow comments, bump version

--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
 // @flow
 
-type ReduxStore = {
-  dispatch: Function,
-  getState: () => Object
-};
-
-export type EffectParams = {
-  action: Object,
-  dispatch: (action: any) => void,
-  getState: () => any,
-  nextDispatchAsync: (actionType: string) => Promise<Object>,
-};
-
-export type EffectErrorHandlerParams = {
-  action: Object,
-  dispatch: (action: any) => void,
-  getState: () => any,
-  nextDispatchAsync: (actionType: string) => Promise<Object>,
-  error: Object,
-};
-
-export type EffectFunction = (params: EffectParams) => Promise<any>;
-
-export type EffectDefinition = {
-  action: string,
-  effect: EffectFunction,
-  error?: (params: EffectErrorHandlerParams) => any
-};
+import type {
+  EffectDefinition,
+  ReduxStore,
+} from './index';
 
 export const effectsMiddleware = (effectsDefinitionArray: Array<EffectDefinition>) => {
   let _waiting = {};
